6436cb72fc74bf5fd0da9c585e5fef0699478692
Lucas Rocha de Jesus <76123986+lucasrochadejesus@users.noreply.github.com>
2022-03-25 20:41:18 -0300

    Data Context

--- Models/Post.cs
+++ Models/Post.cs
@@ -1,12 +1,17 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Blog.Models
 {
+
+     [Table("Post")]
     public class Post
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public int CategoryId { get; set; }
-        public int AuthorId { get; set; }
+      
         public string Title { get; set; }
         public string Summary { get; set; }
         public string Body { get; set; }
@@ -15,5 +20,12 @@ namespace Blog.Models
         public DateTime CreateDate {get;set;}
         public DateTime LastUpdateDate {get;set;}
 
+        [ForeignKey("CategoryId")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public int AuthorId { get; set; }
+        public User Author { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,46 +10,17 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-           using(var context = new DataContext())
+           
+           using var context = new DataContext();
+           
+           var user = new User
            {
-
-                // #### Create
-                // var tag = new Tag{ Name="ASP.NET", Slug="aspnet"};
-                //   context.Tags.Add(tag);
-                //   context.SaveChanges();
-
-                // #### Update
-                // read item from database
-                // var tag = context.Tags.FirstOrDefault(x => x.Id == 5);
-                // tag.Name = ".NET Core";
-                // tag.Slug = ".Net Entity Framework Core.";
-                // context.Update(tag);
-                // context.SaveChanges();
-
-                // ### Delete
-                // var tag = context.Tags.FirstOrDefault(x => x.Id == 5);
-                // context.Remove(tag);
-                // context.SaveChanges();
-
-                // Search
-                // var tags = context.Tags
-                // .Where(x=> x.Name.Contains("Core"))
-                // .ToList();
-
-                // foreach (var tag in tags)
-                // {
-                //     Console.WriteLine(tag.Name);
-                // }
-
-                // var tag = context.Tags
-                // .AsNoTracking()
-                // .FirstOrDefault(x => x.Id == 5);
-                // Console.WriteLine(tag?.Name);
-
-                // Or use Single 
-
-           }
-
+               Name = "Lucas",
+               Slug = "lucasrocha",
+               Email = "lucasfecap@icloud.com",
+               PasswordHash = "1234"
+               
+           };
         }
     }
 }

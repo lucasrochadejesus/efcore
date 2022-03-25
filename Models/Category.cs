4bcefaa054334c6c5532cd858fae841720331f83
Lucas Rocha de Jesus <76123986+lucasrochadejesus@users.noreply.github.com>
2022-03-25 20:29:58 -0300

    Fluent Mapping
    adding Fluent Mapping

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,8 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Blog.Models {
+
+    [Table("Category")]
     public class Category
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        
+        [Required]
+        [MaxLength(80)]
+        [MinLength(1)]
+        [Column("Name", TypeName = "NVARCHAR")]
         public string Name { get; set; }
+
+        [Required]
+        [MaxLength(80)]
+        [MinLength(1)]
+        [Column("Name", TypeName = "VARCHAR")]
         public string Slug { get; set; }
     }
 }

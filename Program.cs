eb8324b300632f08fdc12f798088dbd7b805a575
Lucas Rocha de Jesus <76123986+lucasrochadejesus@users.noreply.github.com>
2022-03-25 11:12:22 -0300

    Delete

--- Program.cs
+++ Program.cs
@@ -13,18 +13,23 @@ namespace Blog
            using(var context = new DataContext())
            {
 
-            // Create
-              // var tag = new Tag{ Name="ASP.NET", Slug="aspnet"};
-              //   context.Tags.Add(tag);
-              //   context.SaveChanges();
+                // #### Create
+                // var tag = new Tag{ Name="ASP.NET", Slug="aspnet"};
+                //   context.Tags.Add(tag);
+                //   context.SaveChanges();
 
-             // Update
-             var tag = context.Tags.FirstOrDefault(x => x.Id == 5);
-             tag.Name = ".NET Core";
-             tag.Slug = ".Net Entity Framework Core.";
-             context.Update(tag);
-             context.SaveChanges();
+                // #### Update
+                // read item from database
+                // var tag = context.Tags.FirstOrDefault(x => x.Id == 5);
+                // tag.Name = ".NET Core";
+                // tag.Slug = ".Net Entity Framework Core.";
+                // context.Update(tag);
+                // context.SaveChanges();
 
+                // ### Delete
+                // var tag = context.Tags.FirstOrDefault(x => x.Id == 5);
+                // context.Remove(tag);
+                // context.SaveChanges();
 
 
            }

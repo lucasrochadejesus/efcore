2a8fd55c6213180618a77e1e6a1d785f12d0305f
Lucas Rocha de Jesus <76123986+lucasrochadejesus@users.noreply.github.com>
2022-03-25 21:12:46 -0300

    null safe exception solved

--- Program.cs
+++ Program.cs
@@ -11,38 +11,51 @@ namespace Blog
         static void Main(string[] args)
         {
            
-           using var context = new DataContext();
+            using var context = new DataContext();
            
-           var user = new User
-           {
-               Name = "Lucas",
-               Slug = "lucasrocha",
-               Email = "lucasfecap@icloud.com",
-               PasswordHash = "1234",
-               Image = "user.png",
-               Bio = "bio"
-           };
+        //    var user = new User
+        //    {
+        //        Name = "Lucas",
+        //        Slug = "lucasrocha",
+        //        Email = "lucasfecap@icloud.com",
+        //        PasswordHash = "1234",
+        //        Image = "user.png",
+        //        Bio = "bio"
+        //    };
 
-           var category = new Category
-           {
-               Name = "Back-end",
-               Slug = "backend"
-           };
+        //    var category = new Category
+        //    {
+        //        Name = "Back-end",
+        //        Slug = "backend"
+        //    };
 
-           var post = new Post
-           {
-               Author = user,
-               Category = category,
-               Body = "<p>Hello Fundamentals</p>",
-               Summary = "Fundamentals EF Core.",
-               Slug = "EF Core",
-               Title = "EF Core Fundamentals",
-               CreateDate = DateTime.Now,
-               LastUpdateDate = DateTime.Now
-           };
+        //    var post = new Post
+        //    {
+        //        Author = user,
+        //        Category = category,
+        //        Body = "<p>Hello Fundamentals</p>",
+        //        Summary = "Fundamentals EF Core.",
+        //        Slug = "EF Core",
+        //        Title = "EF Core Fundamentals",
+        //        CreateDate = DateTime.Now,
+        //        LastUpdateDate = DateTime.Now
+        //    };
 
-           context.Posts.Add(post);
-           context.SaveChanges();
+        //    context.Posts.Add(post);
+        //    context.SaveChanges();
+
+        var posts = context
+            .Posts
+            .AsNoTracking()
+            .Include(x => x.Author)
+            .OrderByDescending(x => x.LastUpdateDate)
+            .ToList();
+
+        foreach (var post in posts)
+        {
+            // null safe.
+            Console.WriteLine($"{post.Title} writted by {post.Author?.Name}");
+        }
 
         }
     }
